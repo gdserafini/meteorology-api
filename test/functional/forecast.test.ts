336d9f210b4bf9b95aa2c08f115fe56378ff0a98
gdserafini <gdinizserafini@gmail.com>
2023-02-20 12:09:23 -0300

    jest config

---
+++ test/functional/forecast.test.ts
@@ -0,0 +1,18 @@
+import supertest from "supertest";
+
+describe('Beach forecast functional tests', () => {
+
+    it('Should return a forecast with just a few times', async() => {
+
+        const {body, status} = await supertest(app).get('/forecast');
+        expect(status).toBe(200);
+        expect(body).toBe([{
+            forecast: 'forecast1'
+        },
+        {
+            forecast: 'forecast2'
+        }]);
+
+    });
+
+});
