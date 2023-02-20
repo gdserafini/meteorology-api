e57e4ac5e40dfdf4c00a3a60e3befebbc1f50975
gdserafini <gdinizserafini@gmail.com>
2023-02-20 14:44:10 -0300

    server config

---
+++ src/code/controller/forcast.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from "@overnightjs/core";
+import { Request, Response } from "express";
+
+@Controller('forecast')
+export class ForcastController {
+
+    @Get('')
+    public getForecastForLoggedUser(_: Request, res: Response): void{
+        res.send([{
+            forecast: 'forecast1'
+        },
+        {
+            forecast: 'forecast2'
+        }]);
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { Application } from 'express';
+import { ForcastController } from './code/controller/forcast.controller';
+import './util/module-alias';
+
+export class SetupServer extends Server{
+
+    constructor(private port = 3000){
+        super();
+    }
+
+    private setupExpress(): void{
+        this.app.use(bodyParser.json());
+        this.setupControllers();
+    }
+
+    public init(): void{
+        this.setupExpress();
+    }
+
+    private setupControllers(): void{
+        const forecastController = new ForcastController();
+        this.addControllers([forecastController]);
+    }
+
+    public getApp(): Application{
+        return this.app;
+    }
+
+}
