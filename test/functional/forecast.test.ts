ee34cca5c7fd7aba0bd77513a146d14ca79384f9
gdserafini <gdinizserafini@gmail.com>
2023-02-20 14:44:32 -0300

    tests config

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,12 +1,10 @@
-import supertest from "supertest";
-
 describe('Beach forecast functional tests', () => {
 
     it('Should return a forecast with just a few times', async() => {
 
-        const {body, status} = await supertest(app).get('/forecast');
+        const {body, status} = await global.testRequest.get('/forecast');
         expect(status).toBe(200);
-        expect(body).toBe([{
+        expect(body).toEqual([{
             forecast: 'forecast1'
         },
         {
