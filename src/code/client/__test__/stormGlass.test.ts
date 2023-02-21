af706d6196b45a9ba4b99dc16a8c28c7fd12b3d3
gdserafini <gdinizserafini@gmail.com>
2023-02-21 13:42:28 -0300

    axios mock updated

--- src/code/client/__test__/stormGlass.test.ts
+++ src/code/client/__test__/stormGlass.test.ts
@@ -3,16 +3,16 @@ import axios from 'axios';
 import stormglass_weather_3_hour from '@test/fixtures/stormglass_weather_3_hour.json';
 import stormglass_normalized_3_hour from '@test/fixtures/stormglass_normalized_3_hour.json';
 
-jest.mock('axios');
-
 describe('StormGlass client', () => {
+    const mockAxios = axios as jest.Mocked<typeof axios>;
+
     it('should return the normalized forecast from the StormGlass service', async () => {
 
-        axios.get = jest.fn().mockResolvedValue(
+        mockAxios.get.mockResolvedValue(
             {data: stormglass_weather_3_hour});
 
-        const stormGlass = new StormGlass(axios);
-        //lat, lon
+        const stormGlass = new StormGlass(mockAxios);
+                                                        //lat, lon
         const response = await stormGlass.fetchPoints(-58.7984, 17.8081);
 
         expect(response).toEqual(stormglass_normalized_3_hour);
