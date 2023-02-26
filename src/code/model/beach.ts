75598a0e3fcea32b8dcde8eb752e3fc968d9dde8
gdserafini <gdinizserafini@gmail.com>
2023-02-26 14:07:32 -0300

    model updated

--- src/code/model/beach.ts
+++ src/code/model/beach.ts
@@ -1,33 +1,36 @@
-import mongoose, { Document} from "mongoose";
+import mongoose from 'mongoose';
 
-export enum BeachPosition{
-    S = 'S', E = 'E', W = 'W', N = 'N'
+export enum GeoPosition {
+  S = 'S', E = 'E', W = 'W', N = 'N',
 }
 
-export interface Beach{
-    name: string,
-    position: BeachPosition,
-    lat: number,
-    lng: number,
-    _id?: string 
+export interface Beach {
+  name: string;
+  position: GeoPosition;
+  lat: number;
+  lng: number;
+}
+
+export interface ExistingBeach extends Beach {
+  id: string;
 }
 
 const schema = new mongoose.Schema(
-{
-    lat: {type: Number, require: true},
-    lng: {type: Number, require: true},
-    name: {type: String, require: true},
-    position: {type: String, require: true}
-},
-{
+  {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    name: { type: String, required: true },
+    position: { type: String, required: true },
+    },
+  {
     toJSON: {
-        transform: (_, ret): void => {
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.__v;
-        }
-    }
-});
+      transform: (_, ret): void => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
-interface BeachModel extends Omit<Beach, '_id'>, Document{}
-export const Beach = mongoose.model<BeachModel>('Beach', schema);
+export const Beach = mongoose.model<Beach>('Beach', schema);
