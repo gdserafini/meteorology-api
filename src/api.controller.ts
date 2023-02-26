3b471566cf1115678e476e1c787379eb932f8b91
gdserafini <gdinizserafini@gmail.com>
2023-02-26 13:28:08 -0300

    renamed

--- src/api.controller.ts
+++ src/api.controller.ts
@@ -1,6 +1,6 @@
 import { ChildControllers, Controller } from '@overnightjs/core';
 import { ForcastController } from '@src/code/controller/forcast.controller';
-import { BeachesController } from '@src/code/controller/beaches';
+import { BeachesController } from '@src/code/controller/beaches.controller';
 
 @Controller('v1')
 @ChildControllers([
