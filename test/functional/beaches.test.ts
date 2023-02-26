75eb91c5dfed6a290dbdd3c6b21f402f9c2ac08a
gdserafini <gdinizserafini@gmail.com>
2023-02-26 14:08:34 -0300

    tests  updated

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,8 @@
+import { Beach } from "@src/code/model/beach";
+
 describe('Beaches functional tests', () => {
+
+    beforeAll(async () => Beach.deleteMany({}));
 
     describe('Create beaches', () => {
 
@@ -17,6 +21,26 @@ describe('Beaches functional tests', () => {
 
             expect(response.status).toBe(201);
             expect(response.body).toEqual(expect.objectContaining(newBeach));
-        })
+        });
+
+        it('Should return 500 when there is a validation error', async () => {
+
+            const newBeach = {
+                lat: 'invalid lat - string',
+                lng: 151.289824,
+                name: 'Manly',
+                position: 'E'
+            };
+
+            const response = await global.testRequest
+                .post('/v1/beaches')
+                .send(newBeach);
+
+            expect(response.status).toBe(500);
+            expect(response.body).toEqual({
+                error: 
+                'Beach validation failed: lat: Cast to Number failed for value "invalid lat - string" (type string) at path "lat"'
+            });
+        });
     })
 })
