3ce456f3e6d9ed9d657dc26c5f01d63230474ed6
gdserafini <gdinizserafini@gmail.com>
2023-02-26 14:08:04 -0300

    db config

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,9 +1,10 @@
 import config, { IConfig } from 'config';
-import { connect as mongooseConnect, connection } from 'mongoose';
+import { connect as mongooseConnect, connection, set } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<void> => {
+  set('strictQuery', false);
   await mongooseConnect(dbConfig.get('mongoUrl'));
 };
 
